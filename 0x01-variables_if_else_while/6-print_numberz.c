f1ff80b589c37c9535a06a43b4951edb0ba93229
Ojolowo-ojuko Olamide <olamideojolowo1@gmail.com>
2022-10-15 15:13:43 -0700

    print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints single digits in base 10
+ * Return: Always 0
+ */
+int main(void)
+{
+	int no;
+
+	for (no = 0; no < 10; no++)
+		putchat((no % 10) + '0');
+
+	putchar('\n');
+
+	return (0);
+}
